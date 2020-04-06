3d049fee3160e4be4c5edca350c34809d6d75dd8
Mctripp <mctripp94@gmail.com>
2020-04-05 20:39:11 -0400

    Add handlebars, character indexing
    Add handlebars template for listing characters
    Add handlebars template for displaying single character
    Add button functionality for editing/deleting/showing characters
    CSS formatting
    Change some forms to dropdowns, textareas

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,6 +4,7 @@
 // const example = require('./example')
 
 const userEvents = require('./users/events.js')
+const charEvents = require('./characters/events.js')
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
@@ -14,4 +15,7 @@ $(() => {
   $('#form-sign-up').on('submit', userEvents.onSignUp)
   $('#form-sign-out').on('submit', userEvents.onSignOut)
   $('#form-change-password').on('submit', userEvents.onChangePassword)
+  $('#form-char-create').on('submit', charEvents.onCreateCharacter)
+  $('#form-char-show').on('submit', charEvents.onShowCharacters)
+  charEvents.addHandlers()
 })
--- assets/scripts/users/ui.js
+++ assets/scripts/users/ui.js
@@ -6,17 +6,15 @@ const getFormFields = require('./../../../lib/get-form-fields.js')
 // Not explaining these next few functions in depth, names
 // explain them well enough
 
-const resetPwForms = () => {
-
+const clearForm = (form) => {
+  $("#" + form).children().val('')
 } //resetPwForms
 
-const swapPage1To2 = () => {
-
-} //swapPage1To2
-
-const swapPage2To1 = () => {
-
-} //swapPage1To2
+const clearAllForms = () => {
+  clearForm("form-sign-in")
+  clearForm("form-sign-up")
+  clearForm("form-change-password")
+}
 
 const displaySuccessMsg = msg => {
 $("#msgs").text(msg)
@@ -36,14 +34,22 @@ $("#msgs").text(responseData)
 const onSignInSuccess = responseData => {
 displaySuccessMsg("Signed in successfully")
 store.user = responseData.user
+clearAllForms()
+$(".signed-in").removeClass("nvisible")
+$(".signed-out").addClass("nvisible")
 } // onSignInSuccess
 
 const onSignOutSuccess = responseData => {
 displaySuccessMsg("Signed out successfully")
+clearAllForms()
+$(".signed-in").addClass("nvisible")
+$(".signed-out").removeClass("nvisible")
+$(".content").empty()
 } // onSignOutSuccess
 
 const onChangePasswordSuccess = responseData => {
 displaySuccessMsg("Changed password successfully")
+clearAllForms()
 } // onChangePasswordSuccess
 
 // FAILURES -------------------------
@@ -58,24 +64,28 @@ const onSignUpFailure = responseData => {
 displayFailureMsg('Sign up failure - ' +
   responseData.status + ': ' +
   responseData.statusText)
+  clearAllForms()
 } // onSignUpFailure
 
 const onSignInFailure = responseData => {
   displayFailureMsg('Sign in failure - ' +
     responseData.status + ': ' +
     responseData.statusText)
+    clearAllForms()
 } // onSignInFailure
 
 const onSignOutFailure = responseData => {
   displayFailureMsg('Sign out failure - ' +
     responseData.status + ': ' +
     responseData.statusText)
+    clearAllForms()
 } // onSignOutFailure
 
 const onChangePasswordFailure = responseData => {
   displayFailureMsg('Change password failure - ' +
     responseData.status + ': ' +
     responseData.statusText)
+    clearAllForms()
 } // onChangePasswordFailure
 
 module.exports = {
