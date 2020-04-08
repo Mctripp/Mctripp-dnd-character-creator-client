3f0a14d3f2b1f0bbdf0a9394f5cb87f94b4ebbea
Mctripp <mctripp94@gmail.com>
2020-04-08 09:48:21 -0400

    Revert

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://char-create-api.herokuapp.com/',
+  production: 'https://ga-proj2-dnd-5e-api.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
